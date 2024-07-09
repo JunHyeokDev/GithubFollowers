7ffd4705c265a3eea3f173724e1e5a3062fbe7f6
JunHyeokDev <fixme1537@gmail.com>
2024-07-09 23:26:56 +0900

    UserInfo_HeaderView_Done

---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Jun Hyeok Kim on 7/9/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    
+}
