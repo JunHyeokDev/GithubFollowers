f629d0f07ed5cbce897d8fa0937ddf7520a7665c
JunHyeokDev <fixme1537@gmail.com>
2024-07-13 17:09:08 +0900

    Polishing_and_Code_Refactoring

--- GithubFollowers/Screens/SearchVC.swift
+++ GithubFollowers/Screens/SearchVC.swift
@@ -13,7 +13,7 @@ class SearchVC: UIViewController {
     let logoImageView = UIImageView()
     let usernameTextField = GFTextField()
     let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
-    
+    var logoImageViewTopConstraint: NSLayoutConstraint!
     
     // MARK: - Computed Property
     var isUsernameEntered : Bool {
@@ -56,10 +56,14 @@ class SearchVC: UIViewController {
     func configureLogoImageView() {
         view.addSubview(logoImageView)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        logoImageView.image = UIImage(named: "gh-logo")!
+        logoImageView.image = Images.ghLogo
+        
+        let topConstraintConstant : CGFloat = DeviceTypes.isiPhone6_to_SE3 ? 20 : 80
+        
+        logoImageViewTopConstraint = logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant)
+        logoImageViewTopConstraint.isActive = true
         
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
